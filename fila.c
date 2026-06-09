58b7a2f755ff4fb746a4d19475a8ac954a7b92a2
Rafael <monteiro.cavalcante@aluno.uece.br>
2026-06-09 14:15:48 -0300

    Colocando lista.c e fila.c

---
+++ fila.c
@@ -0,0 +1,75 @@
+#include "livro.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+#include "fila.h"
+
+Fila *criarFila()
+{
+    Fila *fila = (Fila *)malloc(sizeof(Fila));
+    fila->inicio = NULL;
+    fila->fim = NULL;
+    return fila;
+}
+
+void enfileirarReserva(Fila *fila, Reserva reserva)
+{
+    NoFila *novoNo = (NoFila *)malloc(sizeof(NoFila));
+    if (novoNo == NULL)
+    {
+        fprintf(stderr, "Erro ao alocar memória para novo nó da fila.\n");
+        return;
+    }
+    novoNo->reserva = reserva;
+    novoNo->proximo = NULL;
+
+    if (filaVazia(fila))
+    {
+        fila->inicio = novoNo;
+        fila->fim = novoNo;
+    }
+    else
+    {
+        fila->fim->proximo = novoNo;
+        fila->fim = novoNo;
+    }
+}
+
+Reserva desenfileirarReserva(Fila *fila)
+{
+    if (filaVazia(fila))
+    {
+        fprintf(stderr, "Fila vazia. Não é possível desenfileirar.\n");
+        Reserva reservaVazia = {"", -1}; // Retorna uma reserva vazia ou com código inválido
+        return reservaVazia;
+    }
+
+    NoFila *temp = fila->inicio;
+    Reserva reservaDesenfileirada = temp->reserva;
+
+    fila->inicio = fila->inicio->proximo;
+    if (fila->inicio == NULL)
+    {
+        fila->fim = NULL; // Se a fila ficar vazia, atualiza o fim para NULL
+    }
+
+    free(temp);
+    return reservaDesenfileirada;
+}
+
+int filaVazia(Fila *fila)
+{
+    return fila->inicio == NULL;
+}
+
+void exibirReservas(Fila *fila)
+{
+    NoFila *atual = fila->inicio;
+    while (atual != NULL)
+    {
+        printf("Usuario: %s\n", atual->reserva.nomeUsuario);
+        printf("Codigo do Livro: %d\n", atual->reserva.codigoLivro);
+        printf("-------------------------\n");
+        atual = atual->proximo;
+    }
+}
---
+++ lista.c
@@ -0,0 +1,51 @@
+#include "livro.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+#include "lista.h"
+
+Lista *criarLista()
+{
+    Lista *lista = (Lista *)malloc(sizeof(Lista));
+    lista->inicio = NULL;
+    return lista;
+}
+
+void inserirEmprestimo(Lista *lista, Emprestimo emprestimo)
+{
+    NoLista *novoNo = (NoLista *)malloc(sizeof(NoLista));
+    novoNo->emprestimo = emprestimo;
+    novoNo->proximo = NULL;
+
+    if (lista->inicio == NULL)
+    {
+        lista->inicio = novoNo;
+    }
+    else
+    {
+        NoLista *atual = lista->inicio;
+        while (atual->proximo != NULL)
+        {
+            atual = atual->proximo;
+        }
+        atual->proximo = novoNo;
+    }
+}
+
+void listarEmprestimos(Lista *lista)
+{
+    NoLista *atual = lista->inicio;
+    while (atual != NULL)
+    {
+        printf("Usuario: %s\n", atual->emprestimo.nomeUsuario);
+        printf("Codigo do Livro: %d\n", atual->emprestimo.codigoLivro);
+        printf("Titulo do Livro: %s\n", atual->emprestimo.tituloLivro);
+        printf("-------------------------\n");
+        atual = atual->proximo;
+    }
+}
+
+int listaVazia(Lista *lista)
+{
+    return lista->inicio == NULL;
+}
